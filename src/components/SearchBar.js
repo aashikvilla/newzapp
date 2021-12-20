377b558432cf19f38b16fa73fa2f1292db037bc4
Aashik Villa <aashikvilla99@gmail.com>
2021-12-20 10:06:30 +0530

    removing target="_blank"

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -46,7 +46,7 @@ function SearchBar() {
         <div className="dataResult">
           {filteredData.slice(0, 15).map((value, key) => {
             return (
-              <a className="dataItem" href={value.URL} target="_blank">
+              <a className="dataItem" href={value.URL} /*target="_blank"*/>
                 <p>{value.TITLE} </p>
               </a>
             );
